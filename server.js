0254129a26e0a6336740a9930522372242336333
Daniel Rodrigues <danielrodriguesdrs331@gmail.com>
2020-08-30 18:55:37 -0300

    NEW: First commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const server = express();
+server.use(bodyParser.json());
+server.use(cors());
+
+server.post('/send-mail', async function (req, res) {
+    const transporter = createConnect();
+
+    const mailOptions = createMailOptions(req.body)
+
+    try {
+        await transporter.sendMail(mailOptions);
+
+        return res.json({'status': 200});
+    } catch (e) {
+        return res.json({'status': 500});
+    }
+
+
+});
+
+function createConnect() {
+    return nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: process.env.MAIL_SECURE,
+        auth: {
+            user: process.env.MAIL_AUTH_USER,
+            pass: process.env.MAIL_AUTH_PASS
+        }
+    });
+}
+
+function createMailOptions(data) {
+    const name = data.name ? data.name : 'Without name';
+    const email = data.email ? data.email : 'Without email';
+    const message = data.message ? data.message : 'Without message';
+
+    return {
+        from: process.env.MAIL_AUTH_USER,
+        to: process.env.MAIL_RECEIVE,
+        subject: process.env.MAIL_SUBJECT+" - "+name,
+        html: message+" "+"<br><br> <b>Meu email:</b> "+email
+    }
+}
+
+server.listen(process.env.PORT || 3000);
