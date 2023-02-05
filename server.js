fda5b0d1497d1afd336d00c583b8778dc51a512b
Daniel Silva <danielrodriguesdrs331@gmail.com>
2023-02-04 21:24:41 -0300

    new: change config send mail

--- server.js
+++ server.js
@@ -26,13 +26,11 @@ server.post('/send-mail', async function (req, res) {
 
 function createConnect() {
     return nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: process.env.MAIL_SECURE,
-        auth: {
-            user: process.env.MAIL_AUTH_USER,
-            pass: process.env.MAIL_AUTH_PASS
-        }
+        service: process.env.MAIL_SERVICE, 
+        auth: { 
+           user: process.env.MAIL_AUTH_USER, 
+           pass: process.env.MAIL_AUTH_PASS
+         }
     });
 }
 
